1c299be5c092da5bba98089f1042ae6383c8c760
sanatcodes <sanat.thukral@sap.com>
2023-03-07 13:52:02 +0000

    feat: prediction endpoint added

--- pymongo-fastapi-crud/models.py
+++ pymongo-fastapi-crud/models.py
@@ -1,5 +1,5 @@
 import uuid
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class Category(BaseModel):
@@ -22,5 +22,50 @@ class Category(BaseModel):
                 "likes": 4081644,
                 "comment_count": 13244,
                 "videos": 4
+            }
+        }
+
+
+class Predictions(BaseModel):
+    category_id: int
+    year: int
+    month: int
+    day: int
+    day_of_week: int
+
+    @validator('year')
+    def year_validator(cls, v):
+        if v < 1900 or v > 2100:
+            raise ValueError('year must be between 1900 and 2100')
+        return v
+
+    @validator('month')
+    def month_validator(cls, v):
+        if v < 1 or v > 12:
+            raise ValueError('month must be between 1 and 12')
+        return v
+
+    @validator('day')
+    def day_validator(cls, v, values):
+        year = values.get('year')
+        month = values.get('month')
+        if v < 1 or v > 31:
+            raise ValueError('day must be between 1 and 31')
+        if month in [4, 6, 9, 11] and v > 30:
+            raise ValueError('day must be between 1 and 30 for this month')
+        if month == 2 and (v > 29 or (v == 29 and not year % 4 == 0)):
+            raise ValueError(
+                'day must be between 1 and 29 for this year and month')
+        return v
+
+    class Config:
+        allow_population_by_field_name = False
+        schema_extra = {
+            'example': {
+                "category_id": "1",
+                "year": "2017",
+                "month": "1",
+                "day": "1",
+                "day_of_week": "1"
             }
         }
--- pymongo-fastapi-crud/routes.py
+++ pymongo-fastapi-crud/routes.py
@@ -1,18 +1,13 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
+import pandas as pd
+import pickle
 
-from models import Category
+from models import Category, Predictions
 
 router = APIRouter()
 
-
-# @router.post("/", response_description="Create a new category", status_code=status.HTTP_201_CREATED, response_model=Category)
-# def create_category(request: Request, category: Category = Body(...)):
-#     category = jsonable_encoder(category)
-#     new_category = request.app.database["fypProjectGrowth"].insertMany(category)
-
-#     return {"message": "Categories created successfully", "inserted_ids": new_category.inserted_ids}
 
 @router.post("/", response_description="Create a new category", status_code=status.HTTP_201_CREATED, response_model=Category)
 def create_categories(request: Request, categories: List[Category] = Body(...)):
@@ -41,9 +36,44 @@ def list_categories(request: Request):
     return categories
 
 
-@router.get("/{trending_date}", response_description="Get a single category by id", response_model=Category)
+@router.get("/{trending_date}", response_description="Get a single category by id", response_model=List[Category])
 def find_category(trending_date: str, request: Request):
-    if (category := request.app.database["fypProjectGrowth"].find_one({"trending_date": trending_date})) is not None:
-        return category
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f"Category with date {trending_date} not found")
+    filtered_categoryies = list(
+        request.app.database["fypProjectGrowth"].find({"trending_date": trending_date}))
+    if len(filtered_categoryies) > 0:
+        return filtered_categoryies
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Category with date {trending_date} not found")
+
+
+# import models
+with open('yt-views-0.3.0.pk1', 'rb') as f:
+    viewsModel = pickle.load(f)
+
+with open('yt-likes-0.3.0.pk1', 'rb') as f:
+    likesModel = pickle.load(f)
+
+with open('yt-videos-0.3.0.pk1', 'rb') as f:
+    videosModel = pickle.load(f)
+
+
+@router.post('/predict')
+async def predict_all_endpoint(item: Predictions):
+    try:
+        item_dict = item.dict()
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail='Invalid input data: ' + str(e))
+
+    # Additional input validation logic goes here
+
+    df = pd.DataFrame([item_dict.values()], columns=item_dict.keys())
+    views_prediction = viewsModel.predict(df)
+    likes_prediction = likesModel.predict(df)
+    videos_prediction = videosModel.predict(df)
+    return {
+        'views_prediction': int(views_prediction),
+        'likes_prediction': int(likes_prediction),
+        'videos_prediction': int(videos_prediction)
+    }
